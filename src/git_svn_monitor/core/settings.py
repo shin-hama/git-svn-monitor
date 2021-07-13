ec6f909b9ec8805d249769eaa16b3be80bc97b18
shamada <shamada@jeol.co.jp>
2021-07-13 13:47:39 +0900

    FEAT: Make to setting dictionary to class

--- src/git_svn_monitor/core/settings.py
+++ src/git_svn_monitor/core/settings.py
@@ -1,16 +1,33 @@
+from datetime import datetime
 import json
-from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
-from git_svn_monitor.core.config import PathLike
+from git_svn_monitor.core.config import PathLike, TARGET_DIR
 
 
-def load_settings(path: Optional[PathLike] = None) -> dict[str, Any]:
-    setting_file = path
-    if setting_file is None:
-        setting_file = Path(__file__).resolve().parent / "./settings.json"
+class Setting:
+    def __init__(self, **kwargs: Any) -> None:
+        self.repositories: list[Repository] = kwargs.get("repositories", [])
+        self.email: str = kwargs.get("email", "")
+        self.last_updated: Optional[datetime] = kwargs.get("lastUpdated", None)
 
-    with open(setting_file, mode="r", encoding="utf-8") as f:
-        settings = json.load(f)
+
+class Repository:
+    def __init__(self, **kwargs: Any) -> None:
+        self.name: str = kwargs.get("name", "")
+        self.url: str = kwargs.get("url", "")
+
+
+def decode_settings(data: dict[str, Any]) -> Union[Setting, Repository]:
+    if "repositories" in data:
+        return Setting(**data)
+    else:
+        # nested data
+        return Repository(**data)
+
+
+def load_settings(path: PathLike) -> Setting:
+    with open(path, mode="r", encoding="utf-8") as f:
+        settings = json.load(f, object_hook=decode_settings)
 
     return settings
