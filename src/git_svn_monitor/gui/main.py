26f391bbee131cc6efc92be7704f33d8cb9b1fec
shamada <shamada@jeol.co.jp>
2021-07-15 10:01:13 +0900

    MAINT: Make Settings gui to be updatable email

--- src/git_svn_monitor/gui/main.py
+++ src/git_svn_monitor/gui/main.py
@@ -46,6 +46,24 @@ class RepoEntry(tkinter.Frame):
         delete.bind("<ButtonPress>", self.delete())
 
 
+class EmailEntry(tkinter.Frame):
+    def __init__(self, master: tkinter.Tk) -> None:
+        super().__init__(master)
+
+        self.email = tkinter.StringVar()
+        self.pack()
+        self.create_widget()
+
+    def create_widget(self) -> None:
+        email_label = tkinter.Label(self, text="Enter email")
+        email_label.grid(row=0, column=0)
+        separator = tkinter.Label(self, text=":")
+        separator.grid(row=0, column=1)
+
+        email_entry = tkinter.Entry(self, textvariable=self.email)
+        email_entry.grid(row=0, column=2, ipadx=50)
+
+
 class Buttons(tkinter.Frame):
     def __init__(
         self,
@@ -77,6 +95,8 @@ class Main():
     def __init__(self) -> None:
         self.num = 1
         self.repos: list[RepoEntry] = []
+        self.settings: dict[str, Any] = {}
+        self.email_entry: EmailEntry
 
     def add_row(self) -> None:
         self.repos.append(RepoEntry(self.root, self.delete_row))
@@ -104,8 +124,10 @@ class Main():
                 "url": _url,
             })
 
+        self.settings["repositories"] = _repos
+        self.settings["email"] = self.email_entry.email.get()
         with open(target, mode="w", encoding="utf-8") as f:
-            json.dump({"repositories": _repos}, f, allow_nan=False)
+            json.dump(self.settings, f, allow_nan=False)
 
     def delete_row(self) -> Callable[[Any], None]:
         # Delete row that is pressed button
@@ -123,6 +145,10 @@ class Main():
         # Add and Save button
         self.button = Buttons(self.root, self.load_setting,
                               self.add_row, self.save_json)
+
+        # Email Entry
+        self.email_entry = EmailEntry(self.root)
+
         # Entry labels
         Labels(self.root)
 
@@ -131,9 +157,11 @@ class Main():
     def load_setting(self) -> None:
         target = self.button.target.get()
         with open(target, mode="r", encoding="utf-8") as f:
-            settings = json.load(f)
+            self.settings = json.load(f)
 
-        for setting in settings.get("repositories", []):
+        self.email_entry.email.set(self.settings.get("email", "example@email.co.jp"))
+
+        for setting in self.settings.get("repositories", []):
             _entry = RepoEntry(self.root, self.delete_row)
             _entry.url.set(setting["url"])
             _entry.name.set(setting["name"])
