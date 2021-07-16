c52e0ae0bb00986936225d7b3d57b162732437ad
shamada <shamada@jeol.co.jp>
2021-07-16 15:48:21 +0900

    FEAT: Build message to report commits

--- src/git_svn_monitor/main.py
+++ src/git_svn_monitor/main.py
@@ -3,9 +3,13 @@ from git_svn_monitor.model.git_manager import GitManager
 
 def main() -> None:
     git = GitManager()
-    test = git.parse_latest_commit()
-    for t in test:
-        t.parse_tickets()
+    commits = git.parse_latest_commit()
+
+    comment = f"{len(commits)} commits added from {git.settings.last_updated}"
+    commit_messages = [c.build_message_for_redmine() for c in commits]
+
+    result = "\n\n".join([comment, *commit_messages])
+    print(result)
 
 
 if __name__ == "__main__":
--- src/git_svn_monitor/model/commit_parser.py
+++ src/git_svn_monitor/model/commit_parser.py
@@ -16,3 +16,17 @@ class GitCommit:
         _message = self.commit.message
         results = self.ticket_pattern.findall(_message)
         return results
+
+    def build_message_for_redmine(self) -> str:
+        """ Build message for upload redmine.
+        """
+        # title for the collapsed message
+        _title = f"{self.commit.summary.strip()}: {self.commit.author.name}".strip()
+        # Main containts is able to show when open the collapsed message.
+        _timestamp = f"Datetime: {self.commit.authored_datetime.isoformat()}"
+        _containts = f"{_timestamp}\n\n{self.commit.message.strip()}"
+
+        # need new line at the both start and end of message to define collect collapsed sentence.
+        message = f"{{{{collapse({_title})\n{_containts}\n}}}}"
+
+        return message
